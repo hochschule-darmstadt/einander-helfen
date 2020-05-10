8a40de3ba5141ae118b69a1a6b445e83ec97c5ae
Jan Fiedler <jan.b.fiedler@stud.h-da.de>
2020-05-10 17:55:06 +0200

    Created components for SearchBars

---
+++ app/src/components/ui/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-autocomplete
+            filled
+            rounded
+            color="white"
+            label="z.B. MacherIn"
+            append-icon="search"
+            :items="volunteerTags">
+    </v-autocomplete>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        volunteerTags: [
+            'MacherIn',
+            'DenkerIn',
+            'Jugend Arbeit',
+            'Soziales'
+        ]
+    })
+};
+</script>
+
+<style scoped>
+
+</style>
