3906807c501502c03f88d8a993df603c52471d3a
BaderChouaib <bdr.chouaib@gmail.com>
2020-05-28 03:33:22 +0200

    Privacy: Removed helloworld import statement

--- app/src/views/Privacy.vue
+++ app/src/views/Privacy.vue
@@ -330,7 +330,6 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue";
 
 @Component({})
 export default class Privacy extends Vue {}
