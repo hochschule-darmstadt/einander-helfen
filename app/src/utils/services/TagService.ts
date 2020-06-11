1f742b14068224fafb5a1dff2599549b3b7b7217
BaderChouaib <bdr.chouaib@gmail.com>
2020-06-11 23:56:29 +0200

    Refactoring never ends + dont display empty synonyms

--- app/src/utils/services/TagService.ts
+++ app/src/utils/services/TagService.ts
@@ -1,65 +1,64 @@
-import importedTags from '@/utils/tags/index';
-import Tag from '@/models/tag';
-import store from '@/store/store';
+import importedTags from '@/utils/tags/index'
+import Tag from '@/models/tag'
+import store from '@/store/store'
 
 class TagService {
-    private tags: Tag[] = [];
-    private tagsAsStringArr: string[] = [];
-    private filteredTagsAsStringArr: any;
+  private tags: Tag[] = []
+  private tagsAsStringArr: string[] = []
+  private filteredTagsAsStringArr: any
 
-    /**
-     * The constructor initializes the `Tag` list.
-     */
-    constructor() {
-        this.generateTags();
-        this.removeDuplicateTags(this.tagsAsStringArr);
-        this.filteredTagsAsStringArr.forEach((tag) => {
-            store.commit('addTag', tag);
-        });
-    }
+  /**
+   * The constructor initializes the `Tag` list.
+   */
+  constructor () {
+    this.generateTags()
+    this.removeDuplicateTags(this.tagsAsStringArr)
+    this.filteredTagsAsStringArr.forEach(tag => {
+      store.commit('addTag', tag)
+    })
+  }
 
-    private generateTags(): void {
-        this.splitSynonyms();
-        this.getTagsAsStringArray();
-    }
-    //TODO: I think this ignores tags without synonym. So we dont have those in our list!!
-    private splitSynonyms(): void {
-        importedTags.forEach((tag) => {
-            if (tag.synonyms) {
-                var splittedSynonyms = tag.synonyms.split(',');
-                splittedSynonyms.forEach((element) => {
-                    this.tags.push({
-                        label: tag.label,
-                        synonyms: element.trim(),
-                    });
-                });
-            }
-            //This might solve the above mentioned issue. Needs testing
-            else
-            {
-                this.tags.push({
-                    label: tag.label,
-                    synonyms: "",
-                });
-            }
-        });
-    }
-    private getTagsAsStringArray(): void {
-        this.tags.forEach((element) => {
-            this.tagsAsStringArr.push(element.label);
-            this.tagsAsStringArr.push(element.synonyms);
-        });
-    }
+  private generateTags (): void {
+    this.splitSynonyms()
+    this.getTagsAsStringArray()
+  }
+  //TODO: I think this ignores tags without synonym. So we dont have those in our list!!
+  private splitSynonyms (): void {
+    importedTags.forEach(tag => {
+      if (tag.synonyms) {
+        var splittedSynonyms = tag.synonyms.split(',')
+        splittedSynonyms.forEach(element => {
+          this.tags.push({
+            label: tag.label,
+            synonyms: element.trim()
+          })
+        })
+      }
+      //This might solve the above mentioned issue. Needs testing
+      else {
+        this.tags.push({
+          label: tag.label,
+          synonyms: ''
+        })
+      }
+    })
+  }
+  private getTagsAsStringArray (): void {
+    this.tags.forEach(element => {
+      this.tagsAsStringArr.push(element.label)
+      if (element.synonyms) {
+        this.tagsAsStringArr.push(element.synonyms)
+      }
+    })
+  }
 
-    private removeDuplicateTags(tagsAsStringArr): void {
-        this.filteredTagsAsStringArr = [...new Set(tagsAsStringArr)];
-    }
+  private removeDuplicateTags (tagsAsStringArr): void {
+    this.filteredTagsAsStringArr = [...new Set(tagsAsStringArr)]
+  }
 }
 
-const tagServiceInstance = new TagService();
+const tagServiceInstance = new TagService()
 
-export default tagServiceInstance;
+export default tagServiceInstance
 
-export {
-    tagServiceInstance as TagService,
-};
+export { tagServiceInstance as TagService }
