705c11c1b6db4001ea05377462b432cb2ac2a03a
staedoer <andre.doersch@stud.h-da.de>
2020-06-12 12:59:52 +0200

    Formatting

--- app/src/utils/services/TagService.ts
+++ app/src/utils/services/TagService.ts
@@ -1,64 +1,62 @@
-import importedTags from '@/utils/tags/index'
-import Tag from '@/models/tag'
-import store from '@/store/store'
+import importedTags from '@/utils/tags/index';
+import Tag from '@/models/tag';
+import store from '@/store/store';
 
 class TagService {
-  private tags: Tag[] = []
-  private tagsAsStringArr: string[] = []
-  private filteredTagsAsStringArr: any
+    private tags: Tag[] = [];
+    private tagsAsStringArr: string[] = [];
+    private filteredTagsAsStringArr: any;
 
-  /**
-   * The constructor initializes the `Tag` list.
-   */
-  constructor () {
-    this.generateTags()
-    this.removeDuplicateTags(this.tagsAsStringArr)
-    this.filteredTagsAsStringArr.forEach(tag => {
-      store.commit('addTag', tag)
-    })
-  }
+    /**
+     * The constructor initializes the `Tag` list.
+     */
+    constructor() {
+        this.generateTags();
+        this.removeDuplicateTags(this.tagsAsStringArr);
+        this.filteredTagsAsStringArr.forEach((tag) => {
+            store.commit('addTag', tag);
+        });
+    }
 
-  private generateTags (): void {
-    this.splitSynonyms()
-    this.getTagsAsStringArray()
-  }
+    private generateTags(): void {
+        this.splitSynonyms();
+        this.getTagsAsStringArray();
+    }
 
-  private splitSynonyms (): void {
-    importedTags.forEach(tag => {
-      if (tag.synonyms) {
-        var splittedSynonyms = tag.synonyms.split(',')
-        splittedSynonyms.forEach(element => {
-          this.tags.push({
-            label: tag.label,
-            synonyms: element.trim()
-          })
-        })
-      }
-      
-      else {
-        this.tags.push({
-          label: tag.label,
-          synonyms: ''
-        })
-      }
-    })
-  }
-  private getTagsAsStringArray (): void {
-    this.tags.forEach(element => {
-      this.tagsAsStringArr.push(element.label)
-      if (element.synonyms) {
-        this.tagsAsStringArr.push(element.synonyms)
-      }
-    })
-  }
+    private splitSynonyms(): void {
+        importedTags.forEach((tag) => {
+            if (tag.synonyms) {
+                const splittedSynonyms = tag.synonyms.split(',');
+                splittedSynonyms.forEach((element) => {
+                    this.tags.push({
+                        label: tag.label,
+                        synonyms: element.trim()
+                    });
+                });
+            } else {
+                this.tags.push({
+                    label: tag.label,
+                    synonyms: ''
+                });
+            }
+        });
+    }
+    private getTagsAsStringArray(): void {
+        this.tags.forEach((element) => {
+            this.tagsAsStringArr.push(element.label);
+            if (element.synonyms) {
+                this.tagsAsStringArr.push(element.synonyms);
+            }
+        });
+    }
 
-  private removeDuplicateTags (tagsAsStringArr): void {
-    this.filteredTagsAsStringArr = [...new Set(tagsAsStringArr)]
-  }
+    private removeDuplicateTags(tagsAsStringArr): void {
+        this.filteredTagsAsStringArr = [...new Set(tagsAsStringArr)];
+    }
 }
 
-const tagServiceInstance = new TagService()
+const tagServiceInstance = new TagService();
 
-export default tagServiceInstance
+export default tagServiceInstance;
 
-export { tagServiceInstance as TagService }
+export { tagServiceInstance as TagService };
