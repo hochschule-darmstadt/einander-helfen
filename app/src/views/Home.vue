e33df93f44a80b16b7a412e19c2deebbf01dc110
staedoer <andre.doersch@stud.h-da.de>
2020-06-14 15:03:30 +0200

    Thanks Jenja, MVP <3

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -3,24 +3,15 @@
     <Toolbar />
     <VueSlickCarousel :dots="true" :infinite="true" :autoplay="true" :autoplaySpeed="30000">
       <picture>
-        <source
-          media="(max-width: 768px)"
-          srcset="/images/header/1_phone.jpg"
-        />
+        <source media="(max-width: 768px)" srcset="/images/header/1_phone.jpg" />
         <img src="/images/header/1.jpg" />
       </picture>
       <picture>
-        <source
-          media="(max-width: 768px)"
-          srcset="/images/header/2_phone.jpg"
-        />
+        <source media="(max-width: 768px)" srcset="/images/header/2_phone.jpg" />
         <img src="/images/header/2.jpg" />
       </picture>
       <picture>
-        <source
-          media="(max-width: 768px)"
-          srcset="/images/header/3_phone.jpg"
-        />
+        <source media="(max-width: 768px)" srcset="/images/header/3_phone.jpg" />
         <img src="/images/header/3.jpg" />
       </picture>
     </VueSlickCarousel>
@@ -37,8 +28,9 @@
               append-icon="search"
               item-text="tag"
               autocomplete="off"
-              :items="searchProposals"
+              :items="mySearchProposals"
               @input="addSearchTag"
+              :search-input.sync="currentSearchValue"
             ></v-combobox>
           </v-col>
         </v-row>
@@ -50,8 +42,7 @@
               :items="volunteerCities"
               v-model="selectedCity"
               prepend-inner-icon="place"
-              >Mein Standort</v-autocomplete
-            >
+            >Mein Standort</v-autocomplete>
           </v-col>
 
           <v-col cols="12" md="2">
@@ -59,8 +50,7 @@
               label="Umkreis"
               :items="volunteerRadius"
               v-model="selectedRadius"
-              >Überall</v-autocomplete
-            >
+            >Überall</v-autocomplete>
           </v-col>
         </v-row>
       </v-form>
@@ -69,21 +59,22 @@
         <template v-for="tag in volunteerTags">
           <v-col cols="12" md="2" :key="tag.title">
             <v-hover v-slot:default="{ hover }">
-              <v-card
-                class="mx-auto"
-                :elevation="hover ? 12 : 2"
-                :class="{ 'on-hover': hover }"
-              >
-                  <router-link
-                    style="text-decoration: none; color: inherit;"
-                    :to="{name: 'resultPage', query:{q: tag.title} }"
+              <v-card class="mx-auto" :elevation="hover ? 12 : 2" :class="{ 'on-hover': hover }">
+                <router-link
+                  style="text-decoration: none; color: inherit;"
+                  :to="{name: 'resultPage', query:{q: tag.title} }"
+                >
+                  <v-img
+                    class="white--text align-end mt-10"
+                    height="300px"
+                    :key="tag.title"
+                    :src="tag.img"
                   >
-                    <v-img class="white--text align-end mt-10" height="300px" :key="tag.title" :src="tag.img">
-                      <v-card >
-                    <v-card-title class="justify-center black--text" v-html="tag.title"></v-card-title>
-                      </v-card>
-                   </v-img>
-                 </router-link>
+                    <v-card>
+                      <v-card-title class="justify-center black--text" v-html="tag.title"></v-card-title>
+                    </v-card>
+                  </v-img>
+                </router-link>
               </v-card>
             </v-hover>
           </v-col>
@@ -117,65 +108,81 @@ export default Vue.extend({
     Toolbar
   },
 
-
   data: () => ({
     volunteerTags: [
       {
         title: 'Macher/in',
-        img: require('../../public/images/macherIN.jpeg'),
+        img: require('../../public/images/macherIN.jpeg')
       },
       {
         title: 'Denker/in',
-        img: require('../../public/images/denkerIN.jpeg'),
+        img: require('../../public/images/denkerIN.jpeg')
       },
       {
         title: 'Kommunikative',
-        img: require('../../public/images/jugend.jpeg'),
+        img: require('../../public/images/jugend.jpeg')
       },
       {
         title: 'Soziale',
-        img: require('../../public/images/sozial.jpeg'),
-      },
+        img: require('../../public/images/sozial.jpeg')
+      }
     ],
     volunteerCities: [
       'Main Standort',
       'Darmstadt',
       'Frankfurt am Main',
       'Wiesbaden',
-      'Mainz',
+      'Mainz'
     ],
     volunteerRadius: ['Überall', '5 km', '10 km', '25 km', '50 km'],
     selectedTag: '',
     selectedCity: '',
     selectedRadius: '',
     objectArray: [] as Post[],
+    currentSearchValue: ''
   }),
   computed: {
-    ...mapState(['searchProposals'])
+    ...mapState(['searchProposals']),
+    mySearchProposals(): [] {
+      if (!this.currentSearchValue || this.currentSearchValue.length < 3) {
+        return [];
+      }
+      const searchTerm = this.currentSearchValue;
+      return this.searchProposals
+        .map((element: { tag: string; rank: number }) => {
+          console.log(element);
+          element.rank = element.tag.startsWith(searchTerm)
+            ? 2
+            : element.tag.endsWith(searchTerm)
+            ? 1
+            : 0.5;
+
+          return element;
+        })
+        .sort((a, b) => Math.sign(a.rank - b.rank));
+    }
   },
   methods: {
-    addSearchTag(tag: {tag: string} | string): void {
-      const tagName = typeof tag === 'string'
-              ? tag
-              : tag.tag;
+    addSearchTag(tag: { tag: string } | string): void {
+      const tagName = typeof tag === 'string' ? tag : tag.tag;
 
       this.$router.push({
         name: 'resultPage',
         query: {
           q: tagName,
           city: this.selectedCity,
-          radius: this.selectedRadius,
-        },
+          radius: this.selectedRadius
+        }
       });
-    },
+    }
   }
 });
 </script>
 
 <style>
-  .v-input__slot{
-    margin-bottom: 0;
-  }
+.v-input__slot {
+  margin-bottom: 0;
+}
 
 img {
   width: 100%;
