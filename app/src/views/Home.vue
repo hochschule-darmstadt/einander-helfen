e5f440e3e7b7809cc22b3e2fce35b3cb04034fd3
BaderChouaib <bdr.chouaib@gmail.com>
2020-06-27 15:29:13 +0200

    adjust alignment of SearchButton

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -51,10 +51,10 @@
                       <location-search-bar />
                     </v-col>
 
-                    <v-col cols="8" sm="9" md="4">
+                    <v-col cols="8" sm="9" md="4" class="pl-10">
                       <radius />
                     </v-col>
-                    <v-col cols="4" sm="3" md="2">
+                    <v-col cols="4" sm="3" md="2" class="pl-10 py-1">
                       <search-button @click="executeSearch" />
                     </v-col>
                   </v-row>
