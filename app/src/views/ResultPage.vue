3a1aff25af961c09d798a68d8c0bddeb044c88e9
BaderChouaib <bdr.chouaib@gmail.com>
2020-07-08 13:26:09 +0200

    use span tag

--- app/src/views/ResultPage.vue
+++ app/src/views/ResultPage.vue
@@ -165,7 +165,7 @@
                     </v-list-item-title>
                     <v-list-item-subtitle :set="distance = postDistance(post)">
                       <strong>{{post.location}} <em v-if="distance">(in {{distance}})</em></strong> &mdash;
-                       <tag v-html="post.task"/>
+                       <span v-html="post.task"/>
                     </v-list-item-subtitle>
                   </v-list-item-content>
 
