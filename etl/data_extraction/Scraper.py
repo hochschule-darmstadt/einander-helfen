c48a3d63617a72b2f98089a2eb3594b1b1d596ac
Jamie A <JamieArmstrongArndt@users.noreply.github.com>
2020-07-11 17:19:58 +0200

    Fixed duplicate array values in Scraper sub-classes + added logs to .gitignore
    Subclasses of Scraper shared the three initialized arrays which led to duplicate values in these arrays

--- etl/data_extraction/Scraper.py
+++ etl/data_extraction/Scraper.py
@@ -9,9 +9,6 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Scraper:
-    # Scraper name -> Overwritten by name of the scraper file
-    name = 'scraper'
-
     # Domain to be scraped on
     base_url = 'http://example.com'
 
@@ -21,17 +18,18 @@ class Scraper:
     # Delay between requests
     delay = 0.5
 
-    # The URLs which will be parsed and scraped  
-    urls = []
-
-    # And array of the data of the scraped pages (each entry holds the scraped and parsed information of a detail page)
-    data = []
-
-    # An error object to keep track of error occurences (which is used for logging)
-    errors = []
-
     def __init__(self, name):
+        # Scraper name -> Overwritten by name of the scraper file
         self.name = name
+        
+        # The URLs which will be parsed and scraped  
+        self.urls = []
+        
+        # And array of the data of the scraped pages (each entry holds the scraped and parsed information of a detail page)
+        self.data = []
+        
+        # An error object to keep track of error occurences (which is used for logging)
+        self.errors = []
 
     # Runs the Scraper 
     # Step 1: Adding URLs
