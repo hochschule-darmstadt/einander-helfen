cf75f80d0bce50c2a12529af6a62bda56ed447f4
Marvin Oliver Seith <mseith@cai-einander-helfen.fbi.h-da.de>
2020-07-12 14:28:32 +0200

    added script to get scraped data from staging and move to correct position in files for index process

---
+++ etl/scripts/get_data_from_staging.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# change working dir
+cd /home/mseith
+
+# remove old data
+rm -r staging_data
+
+# make data dir
+mkdir staging_data
+
+# switch dir
+cd staging_data
+
+# remove old data file
+rm enhanced_output.tar.gz
+
+# get new file from staging
+wget -4 http://cai-einander-helfen-staging.fbi.h-da.de/enhanced_output.tar.gz
+
+# extract data from folder
+tar xfvz enhanced_output.tar.gz
+
+# delete old data
+rm -r /home/mseith/einander-helfen/etl/data_enhancement/data
+rm -r /home/mseith/einander-helfen/etl/data_enhancement/output
+
+# move data to data_exraction folder
+cp -r data_enhancement/data /home/mseith/einander-helfen/etl/data_enhancement
+cp -r data_enhancement/output /home/mseith/einander-helfen/etl/data_enhancement
