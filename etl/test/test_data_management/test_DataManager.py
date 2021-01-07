b2536941d16cb8f155075f2f40906843fd428eff
Jonas Winkel <jonas.winkel@stud.h-da.de>
2021-01-07 02:07:36 +0100

    #175 added testing fpr DataManager, added logfolders of tests to gitignore

---
+++ etl/test/test_data_management/test_DataManager.py
@@ -0,0 +1,19 @@
+import unittest
+from data_management.DataManager import DataManager
+
+
+class TestDataManager(unittest.TestCase):
+
+    def test_timestamp_to_datestring(self):
+        timestamp = 1577833200.0
+        datestring = "01.01.2020"
+        self.assertEqual(datestring, DataManager.timestamp_to_datestring(timestamp))
+
+    def test_datestring_to_timestamp(self):
+        timestamp = 1577833200.0
+        datestring = "01.01.2020"
+        self.assertEqual(timestamp, DataManager.datestring_to_timestamp(datestring))
+
+
+if __name__ == '__main__':
+    unittest.main()
