b55fef47872232ad174efcacb3e991fbb9d32e1c
Marcel Moravek <marcel.moravek@stud.h-de.de>
2021-04-16 13:23:07 +0200

    Remove unused dependencies & change tslint to eslint

--- app/src/components/ui/AreaSelect.vue
+++ app/src/components/ui/AreaSelect.vue
@@ -1,92 +1,101 @@
 <template>
   <v-row justify="center">
-    <v-select 
-      id="areaSelect" 
-      :items="items" 
+    <v-select
+      id="areaSelect"
+      :items="items"
       v-model="selection"
-      label="" 
-      item-text="title" 
+      label=""
+      item-text="title"
       item-value="title"
       :dark="dark"
-      @change="$emit('change')">
+      @change="$emit('change')"
+    >
       <template v-slot:item="{ item }">
-        <img id="areaImage" :src="item.img">
+        <img id="areaImage" :src="item.img" />
         <v-spacer></v-spacer>
-        <span>{{item.title}}</span>
+        <span>{{ item.title }}</span>
       </template>
       <template v-slot:selection="{ item }">
-        <img id="areaImageSelected" :src="item.img">
+        <img id="areaImageSelected" :src="item.img" />
       </template>
     </v-select>
   </v-row>
 </template>
 
 <script lang="ts">
-import { createNamespacedHelpers, mapActions as mapStateActions, mapGetters } from 'vuex';
-const { mapState, mapActions } = createNamespacedHelpers('locationSearchModule');
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import { Prop, Watch } from 'vue-property-decorator';
-import { Getter, Mutation } from 'vuex-class';
+import Vue from "vue";
 
 interface Item {
   title: string;
   img: string;
 }
 
-@Component
-export default class AreaSelect extends Vue {
-
-  get international(): boolean {
-    return this.getInternational;
-  }
-  @Getter('getInternational') public getInternational;
-  @Mutation('setInternational') public setInternational;
-
-  @Prop({default: false})
-  public dark!: boolean;
-
-  @Prop({default: ''})
-  public attachTo!: string;
-
-  public items: Item[] = [
-    {
-      title: 'Deutschland',
-      img: '/images/240px-Flag_of_Germany.png'
+export default Vue.extend({
+  name: "AreaSelect",
+  props: {
+    dark: {
+      type: Boolean,
+      default: false,
     },
-    {
-      title: 'International',
-      img: '/images/240px-Earth_icon_2.png'
-    }
-  ];
-
-  public selection: string = this.items[0].title;
-
-  @Watch('selection')
-  public onPropertyChanged(newValue: string): void {
-    if (newValue === this.items[0].title) {
-      this.setInternational(false);
-    } else {
-      this.setInternational(true);
-    }
-  }
-
-  public mounted(): void {
+    attachTo: {
+      type: String,
+      default: "",
+    },
+  },
+  data: function () {
+    return {
+      items: [
+        {
+          title: "Deutschland",
+          img: "/images/240px-Flag_of_Germany.png",
+        },
+        {
+          title: "International",
+          img: "/images/240px-Earth_icon_2.png",
+        },
+      ] as Item[],
+      selection: "",
+    };
+  },
+  mounted(): void {
+    this.selection = this.items[0].title;
     this.setSelection(this.international);
-  }
-
-  public setSelection(international: boolean): void {
-    if (international) {
-      this.selection = this.items[1].title;
-    } else {
-      this.selection = this.items[0].title;
-    }
-  }
-}
+  },
+  computed: {
+    getInternational(): any {
+      return this.$store.getters.getInternational;
+    },
+    international(): boolean {
+      return this.getInternational;
+    },
+  },
+  watch: {
+    selection(newValue: string): void {
+      if (newValue === this.items[0].title) {
+        this.setInternational(false);
+      } else {
+        this.setInternational(true);
+      }
+    },
+  },
+  methods: {
+    setSelection(international: boolean): void {
+      if (international) {
+        this.selection = this.items[1].title;
+      } else {
+        this.selection = this.items[0].title;
+      }
+    },
+    setInternational(obj: Boolean) {
+      this.$store.commit("setInternational", obj);
+    },
+  },
+});
 </script>
 
-<style>
-#areaImage, #areaImageSelected {
+<style lang="scss">
+#areaImage,
+#areaImageSelected {
   height: 1.5em;
   width: 1.5em;
   object-fit: cover;
@@ -102,13 +111,12 @@ export default class AreaSelect extends Vue {
   visibility: hidden;
 }
 
-#areaSelect{
+#areaSelect {
   margin-top: 4px;
   max-width: fit-content;
   margin-right: 8px;
   margin-left: 0;
 }
-
 </style>
 
 
