0a71a0cefc979b2dd2380cccd90869e2db185f4d
theErikss0n <github@der-klinger.de>
2021-06-03 13:24:32 +0200

    Initial lint

--- app/src/services/PostService.ts
+++ app/src/services/PostService.ts
@@ -24,8 +24,8 @@ class PostService {
   private baseUrl = process.env.VUE_APP_SEARCH_URI;
 
   /**
-   * Return a paginated list of posts fitting to the @SearchParameters 
-   * @param params @SearchParameters 
+   * Return a paginated list of posts fitting to the @SearchParameters
+   * @param params @SearchParameters
    * @returns Promise<PaginatedResponse<Post>>
    */
   public findPosts(params: SearchParameters): Promise<PaginatedResponse<Post>> {
@@ -36,38 +36,32 @@ class PostService {
         params.searchValues
           .map((value) => value + "*")
           .map((value) =>
-            builder.orQuery("query_string",
-              {
-                query: value,
-                fields: ["title", "categories", "task"]
-              },
-            )
-          )
+            builder.orQuery("query_string", {
+              query: value,
+              fields: ["title", "categories", "task"],
+            })
+          );
         return builder;
-      })
+      });
 
-    builder = params.international ?
-      this.addInternationalFilter(builder, params.location) :
-      this.addNationalFilter(builder, params.location, params.radius);
+    builder = params.international
+      ? this.addInternationalFilter(builder, params.location)
+      : this.addNationalFilter(builder, params.location, params.radius);
 
     return this.performPostsQuery<Post>(builder);
   }
 
-
   public findById(id: string): Promise<Post | undefined> {
-    const builder = BuilderFactory()
-      .query('term', "_id", id);
+    const builder = BuilderFactory().query("term", "_id", id);
 
-    return axios
-      .post(this.baseUrl, builder.build())
-      .then(({ data }) => {
-        if (!data.hits.hits.length) return undefined;
-        const entity = data.hits.hits.pop();
-        return {
-          id: entity._id,
-          ...entity._source,
-        } as Post;
-      });
+    return axios.post(this.baseUrl, builder.build()).then(({ data }) => {
+      if (!data.hits.hits.length) return undefined;
+      const entity = data.hits.hits.pop();
+      return {
+        id: entity._id,
+        ...entity._source,
+      } as Post;
+    });
   }
 
   /**
@@ -76,12 +70,11 @@ class PostService {
    * @returns Promise<number>
    */
   public countPosts(international: boolean): Promise<number> {
-    let builder = BuilderFactory()
-      .rawOption("track_total_hits", true)
+    let builder = BuilderFactory().rawOption("track_total_hits", true);
 
-    builder = international ?
-      this.addInternationalFilter(builder) :
-      this.addNationalFilter(builder);
+    builder = international
+      ? this.addInternationalFilter(builder)
+      : this.addNationalFilter(builder);
 
     return this.performCountQuery(builder);
   }
@@ -95,30 +88,31 @@ class PostService {
     builder = builder.notQuery("term", "categories", "international");
 
     if (location) {
-      builder = builder.sort([{
-        _geo_distance: {
-          geo_location: {
-            lat: location.lat,
-            lon: location.lon,
+      builder = builder.sort([
+        {
+          _geo_distance: {
+            geo_location: {
+              lat: location.lat,
+              lon: location.lon,
+            },
+            order: "asc",
+            unit: "km",
+            mode: "min",
+            distance_type: "arc",
+            ignore_unmapped: true,
           },
-          order: "asc",
-          unit: "km",
-          mode: "min",
-          distance_type: "arc",
-          ignore_unmapped: true,
         },
-      }]);
+      ]);
     }
 
     if (location && radius) {
-      builder = builder.filter("geo_distance",
-        {
-          distance: radius,
-          geo_location: {
-            lat: location.lat,
-            lon: location.lon,
-          },
-        });
+      builder = builder.filter("geo_distance", {
+        distance: radius,
+        geo_location: {
+          lat: location.lat,
+          lon: location.lon,
+        },
+      });
     }
 
     return builder;
@@ -133,43 +127,46 @@ class PostService {
 
     if (location) {
       if (location.country && location.country !== "Deutschland") {
-        builder = builder.andQuery("match", "post_struct.location.country.keyword", location.country);
+        builder = builder.andQuery(
+          "match",
+          "post_struct.location.country.keyword",
+          location.country
+        );
       }
     }
 
     return builder;
   }
 
-  private performPostsQuery<T>(query: Bodybuilder): Promise<PaginatedResponse<T>> {
-    return axios
-      .post(this.baseUrl, query.build())
-      .then(({ data }) => {
-        const entities: T[] = data.hits.hits.map((elem: any) => {
-          return {
-            id: elem._id,
-            ...elem._source,
-          };
-        });
-
-        const response: PaginatedResponse<T> = {
-          data: entities,
-          meta: {
-            total: data.hits.total.value,
-            size: entities.length,
-          },
+  private performPostsQuery<T>(
+    query: Bodybuilder
+  ): Promise<PaginatedResponse<T>> {
+    return axios.post(this.baseUrl, query.build()).then(({ data }) => {
+      const entities: T[] = data.hits.hits.map((elem: any) => {
+        return {
+          id: elem._id,
+          ...elem._source,
         };
-        return response;
-      })
-  }
+      });
 
+      const response: PaginatedResponse<T> = {
+        data: entities,
+        meta: {
+          total: data.hits.total.value,
+          size: entities.length,
+        },
+      };
+      return response;
+    });
+  }
 
   private performCountQuery<T>(query: Bodybuilder): Promise<number> {
     return axios
       .post(this.baseUrl + "posts/?filter_path=hits.total", query.build())
       .then(({ data }) => {
-        const count: number = data.hits.total.value
+        const count: number = data.hits.total.value;
         return count;
-      })
+      });
   }
 }
 
