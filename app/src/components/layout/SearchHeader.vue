31d06ab2081920ec81cb251209058d9241dc2f2f
MMoravek <xcaverxiii@yahoo.de>
2021-06-21 20:44:15 +0200

    #395 implement header expansion

--- app/src/components/layout/SearchHeader.vue
+++ app/src/components/layout/SearchHeader.vue
@@ -1,30 +1,41 @@
 <!-- This Header contains the logo, all components for searching and the burger menu. -->
 
 <template>
-  <header class="header_layout">
-    <v-btn
-      class="d-none d-sm-flex justify-center mr-4"
-      height="70px"
-      width="80px"
-      justify="left"
-      rounded
-      router
-      depressed
-      icon
-    >
-      <router-link to="/" exact>
-        <v-img
-          class="mt-1"
-          width="80px"
-          height="75px"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Heart-hand-shake.svg/256px-Heart-hand-shake.svg.png"
-        />
-      </router-link>
-    </v-btn>
+  <header class="header" :class="{ expanded: expanded }">
+    <div class="header_layout">
+      <v-btn
+        class="d-none d-sm-flex justify-center mr-4"
+        height="70px"
+        width="80px"
+        justify="left"
+        rounded
+        router
+        depressed
+        icon
+      >
+        <router-link to="/" exact>
+          <v-img
+            class="mt-1"
+            width="80px"
+            height="75px"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Heart-hand-shake.svg/256px-Heart-hand-shake.svg.png"
+          />
+        </router-link>
+      </v-btn>
 
-    <SearchComponent class="search_bar" dark direktsearch small />
+      <SearchComponent
+        class="search_bar"
+        dark
+        direktsearch
+        small
+        :expanded="expanded"
+      />
 
-    <MenuButton />
+      <MenuButton />
+    </div>
+    <div class="expantion_area d-sm-none" @click="expanded = !expanded">
+      <v-icon class="icon" large>expand_more</v-icon>
+    </div>
   </header>
 </template>
 
@@ -39,21 +50,49 @@ export default Vue.extend({
     SearchComponent,
     MenuButton,
   },
-  methods: {},
+  data: function () {
+    return {
+      expanded: false,
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
-.header_layout {
-  padding: 12px;
+.header {
+  padding: 12px 12px 0 12px;
   background: #00254f;
-  display: flex;
-  flex-direction: row;
   width: 100%;
+  // TODO: add transition
 
-  .search_bar {
-    flex-grow: 1;
-    margin-right: 30px;
+  .header_layout {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+
+    .search_bar {
+      flex-grow: 1;
+      margin-right: 30px;
+    }
+  }
+
+  .expantion_area {
+    text-align: center;
+    width: 100%;
+    margin-top: -5px;
+    .icon {
+      color: white;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    $radius: 17px;
+    border-bottom-left-radius: $radius;
+    border-bottom-right-radius: $radius;
+
+    &.expanded .expantion_area .icon {
+      transform: rotate(180deg);
+    }
   }
 }
 </style>
