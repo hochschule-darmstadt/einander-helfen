5d374162416cab43ea5c20b17514565543cc45f6
Arnold Porada <arnold.porada@stud.h-da.de>
2021-11-16 12:36:06 +0100

    Change imprint for USA flag in drop down language

--- app/src/views/Imprint.vue
+++ app/src/views/Imprint.vue
@@ -51,12 +51,22 @@
       <h1>{{ $t("imprint.sourceReference") }}</h1>
       <ul class="mt-2 mb-5">
         <li v-for="(item, index) in items" :key="index" class="mb-2">
-          <strong>{{ item.author }}</strong> - <i>{{ item.position }}</i> - 
+           - <!--use i18n translation if exists
+          
+            <strong v-if='$te("imprint.sourceReferenceItems."+item.i18nKey+".author")'>{{ $t("imprint.sourceReferenceItems."+item.i18nKey+".author") }}</strong>
+          <strong v-else>{{ item.author }}</strong>
+          -->
+           <strong>{{ item.author }}</strong>
+           - <!--use i18n translation if exists
+          <i v-if='$te("imprint.sourceReferenceItems."+item.i18nKey+".position")'>{{ $t("imprint.sourceReferenceItems."+item.i18nKey+".position") }}</i> 
+          <i v-else>{{ item.position }}</i> -->
+          <i >{{ item.position }}</i> - 
           <br />
-          <a class="wordbreak" :href="item.source">{{ item.source }}</a
-          >,
-          <!--use i18n translation if exists-->
-          <p class="wordbreak" >{{ item.license }}</p>
+          <!--use i18n translation if exists
+          <p class="wordbreak" v-if='$te("imprint.sourceReferenceItems."+item.i18nKey+".license")'>{{ $t("imprint.sourceReferenceItems."+item.i18nKey+".license") }}</p>
+          <p class="wordbreak" v-else>{{ item.license }}</p>
+          -->
+          <p class="wordbreak">{{ item.license }}</p>
         </li>
       </ul>
     </section>
@@ -75,6 +85,7 @@ export default Vue.extend({
     return {
       items: [
         {
+          //i18nKey:"iitBombay",
           position: this.$t("imprint.sourceReferenceItems.iitBombay.position"),
           author: "IIT Bombay",
           source:
@@ -83,6 +94,7 @@ export default Vue.extend({
             //"The copyright holder of this file, IIT Bombay, allows anyone to use it for any purpose, provided that the copyright holder is properly attributed. Redistribution, derivative work, commercial use, and all other use is permitted.",
         },
         {
+          //i18nKey:"sKopp",
           position:this.$t("imprint.sourceReferenceItems.sKopp.position"),
           author: this.$t("imprint.sourceReferenceItems.sKopp.author"),
           source: "https://commons.wikimedia.org/wiki/File:Flag_of_Germany.svg",
@@ -90,6 +102,7 @@ export default Vue.extend({
             //"This image of simple geometry is ineligible for copyright and therefore in the public domain, because it consists entirely of information that is common property and contains no original authorship.",
         },
         {
+          //i18nKey:"tommasoSansone",
           position: this.$t("imprint.sourceReferenceItems.tommasoSansone.position"),
           author: "Tommaso.sansone91",
           source: "https://commons.wikimedia.org/wiki/File:Earth_icon_2.png",
@@ -97,6 +110,7 @@ export default Vue.extend({
             //"This file is made available under the Creative Commons CC0 1.0 Universal Public Domain Dedication. The person who associated a work with this deed has dedicated the work to the public domain by waiving all of their rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law. You can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission.",
         },
         {
+          //i18nKey:"truthseeker",
           position: this.$t("imprint.sourceReferenceItems.truthseeker.position"),
           author: "truthseeker08",
           source:
@@ -104,6 +118,7 @@ export default Vue.extend({
           license: "https://pixabay.com/de/service/terms/#license",
         },
         {
+          //i18nKey:"timkraaijvanger",
           position: this.$t("imprint.sourceReferenceItems.timkraaijvanger.position"),
           author: "timkraaijvanger",
           source:
@@ -111,6 +126,7 @@ export default Vue.extend({
           license: "https://pixabay.com/de/service/terms/#license",
         },
         {
+          //i18nKey:"anemone",
           position: this.$t("imprint.sourceReferenceItems.anemone.position"),
           author: "Anemone123",
           source:
@@ -118,6 +134,7 @@ export default Vue.extend({
           license: "https://pixabay.com/de/service/terms/#license",
         },
         {
+          //i18nKey:"cottonbro",
           position: this.$t("imprint.sourceReferenceItems.cottonbro.position"),
           author: "cottonbro",
           source:
@@ -125,6 +142,7 @@ export default Vue.extend({
           license: "https://www.pexels.com/de-de/lizenz/",
         },
         {
+          //i18nKey:"andreaPiacquadio",
           position: this.$t("imprint.sourceReferenceItems.andreaPiacquadio.position"),
           author: "Andrea Piacquadio",
           source:
@@ -132,6 +150,7 @@ export default Vue.extend({
           license: "https://www.pexels.com/de-de/lizenz/",
         },
         {
+          //i18nKey:"fauxels",
           position: this.$t("imprint.sourceReferenceItems.fauxels.position"),
           author: "fauxels",
           source:
@@ -139,6 +158,7 @@ export default Vue.extend({
           license: "https://www.pexels.com/de-de/lizenz/",
         },
         {
+          //i18nKey:"andreaPiacquadio2",
           position: this.$t("imprint.sourceReferenceItems.andreaPiacquadio2.position"),
           author: "Andrea Piacquadio",
           source:
@@ -146,10 +166,19 @@ export default Vue.extend({
           license: "https://www.pexels.com/de-de/lizenz/",
         },
         {
+          //i18nKey:"freepik",
           position: this.$t("imprint.sourceReferenceItems.freepik.position"),
           author: "https://www.freepik.com",
           source: "https://www.flaticon.com/free-icon/robot_3398613",
           license: "https://www.freepikcompany.com/legal",
+        },
+        {
+          //i18nKey:"dbenbenn",
+          position: this.$t("imprint.sourceReferenceItems.dbenbenn.position"),
+          author: "Dbenbenn, Zscout370, Jacobolus, Indolences, Technion.",
+          source: "https://commons.wikimedia.org/wiki/File:Flag_of_the_United_States.svg",
+          license:this.$t("imprint.sourceReferenceItems.dbenbenn.license")
+            //"This image is ineligible for copyright and therefore is in the public domain, because it consists entirely of information that is common property and contains no original authorship.",
         },
       ],
     };
