e92e7194831603505b8770ec34a5ee604d70312b
Granoffel <marcwei100@gmail.com>
2021-11-24 10:48:22 +0100

    added alt atributes to all non decorative images (#475)
    * added alt atributes to all non decorativ images
    
    * #468 rename the alt attributes to "Flagge-Deutschland" and "Flagge-International"
    
    Co-authored-by: Marc Weidenfeller <marc.weidenfeller@stud.h-da.de>

--- app/src/components/layout/Carousel.vue
+++ app/src/components/layout/Carousel.vue
@@ -9,15 +9,15 @@
   >
     <picture>
       <source media="(max-width: 768px)" srcset="/images/header/1_phone.jpg" />
-      <img src="/images/header/1.jpg" />
+      <img alt="Seniorenbetreuung" src="/images/header/1.jpg" />
     </picture>
     <picture>
       <source media="(max-width: 768px)" srcset="/images/header/2_phone.jpg" />
-      <img src="/images/header/2.jpg" />
+      <img alt="Arbeit mit Kindern" src="/images/header/2.jpg" />
     </picture>
     <picture>
       <source media="(max-width: 768px)" srcset="/images/header/3_phone.jpg" />
-      <img src="/images/header/3.jpg" />
+      <img alt="Teamwork" src="/images/header/3.jpg" />
     </picture>
   </VueSlickCarousel>
 </template>
